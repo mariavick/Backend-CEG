f8d2cdb0d124c69995dc5cfed4315285c21262fb
Maria Vitória <mariavivibibi120@gmail.com>
2024-05-03 16:08:48 -0300

    correction of functions in the user table

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { userRoute } from "./routes/user.routes.js";
 
 export const app = express();
 const port = 7575;
 
 app.use(express.json());
+app.use(userRoute)
 
 app.listen(port, async () => {
     console.log(`Servidor tá funcionando port ${port}`)
---
+++ src/utils/response.js
@@ -0,0 +1,6 @@
+export const SUCCESS = {
+    DELETED: "Usuário deletado"}
+
+export const ERRORS = {
+    NOT_FOUND:"Usuario não encontrado"
+} 
